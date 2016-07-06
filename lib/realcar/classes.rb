f04e418ffe809531d8b57dbf9a9261b39f2f4393
Alexander Morozov <oleksandr.morozov@customertimes.com>
2016-07-06 18:01:51 +0300

    New class

--- lib/realcar/classes.rb
+++ lib/realcar/classes.rb
@@ -2,7 +2,7 @@ require 'active_support'
 
 module Realcar
 
-  CLASSES = ['Cars', 'PricingRules', 'Services']
+  CLASSES = ['Cars', 'PricingRules', 'Services', 'Settings']
 
   CLASSES.each do |c|
     Realcar.const_set c, Class.new(Client) { self.resource_name = ActiveSupport::Inflector.tableize(c) }
